60802c97e1665ec31d8c45af70c267a07227e019
intks <30889157+Intks@users.noreply.github.com>
2026-04-26 15:35:10 +0800

    feat: enhance notes metadata by adding new entries

--- apps/docs/pages/notes/_meta.js
+++ apps/docs/pages/notes/_meta.js
@@ -1,5 +1,8 @@
 export default {
+  'ban-use-effect': 'Ban useEffect',
   'component-design': 'Component Design',
   'enum-vs-string-union': 'Enum vs String Union',
+  'file-upload': 'File Upload',
+  'what-is-generics': 'What is Generics',
+  'what-is-code-review': 'What is Code Review',
 }
-
